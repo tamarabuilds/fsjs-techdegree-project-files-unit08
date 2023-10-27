2a0b63dd48e2769842df1265aa98fb26109bfa16
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-10-27 10:53:15 -0700

    initial sequelize orm workshop files

---
+++ sequelize-orm-with-express-workshop/2-Install-and-Use-Sequelize-CLI/using-sequelize-orm-with-express/models/article.js
@@ -0,0 +1,13 @@
+'use strict';
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Article extends Sequelize.Model {}
+  Article.init({
+    title: Sequelize.STRING,
+    author: Sequelize.STRING,
+    body: Sequelize.TEXT
+  }, { sequelize });
+
+  return Article;
+};
---
+++ sequelize-orm-with-express-workshop/6-Update-and-Delete-Entries/using-sequelize-orm-with-express/routes/articles.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const Article = require('../models').Article;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      res.status(500).send(error);
+    }
+  }
+}
+
+/* GET articles listing. */
+router.get('/', asyncHandler(async (req, res) => {
+  const articles = await Article.findAll({ order: [["createdAt", "DESC"]] });
+  res.render("articles/index", { articles, title: "Sequelize-It!" });
+}));
+
+/* Create a new article form. */
+router.get('/new', (req, res) => {
+  res.render("articles/new", { article: {}, title: "New Article" });
+});
+
+/* POST create article. */
+router.post('/', asyncHandler(async (req, res) => {
+  const article = await Article.create(req.body);
+  res.redirect("/articles/" + article.id);
+}));
+
+/* Edit article form. */
+router.get("/:id/edit", asyncHandler(async(req, res) => {
+  const article = await Article.findByPk(req.params.id);
+  res.render("articles/edit", { article, title: "Edit Article" });
+}));
+
+/* GET individual article. */
+router.get("/:id", asyncHandler(async (req, res) => {
+  const article = await Article.findByPk(req.params.id);
+  res.render("articles/show", { article, title: article.title }); 
+})); 
+
+/* Update an article. */
+router.post('/:id/edit', asyncHandler(async (req, res) => {
+  const article = await Article.findByPk(req.params.id);
+  await article.update(req.body);
+  res.redirect("/articles/" + article.id);
+}));
+
+/* Delete article form. */
+router.get("/:id/delete", asyncHandler(async (req, res) => {
+  const article = await Article.findByPk(req.params.id);
+  res.render("articles/delete", { article, title: "Delete Article" });
+}));
+
+/* Delete individual article. */
+router.post('/:id/delete', asyncHandler(async (req ,res) => {
+  const article = await Article.findByPk(req.params.id);
+  await article.destroy();
+  res.redirect("/articles");
+}));
+
+module.exports = router;
